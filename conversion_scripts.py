18404c82f35dfa34da33ce6f686f4e4a1bf2da7a
Alex Goodell <alexgoodell@gmail.com>
2024-03-06 03:57:29 -0800

    very successful with pyhtml2pdf

--- conversion_scripts.py
+++ conversion_scripts.py
@@ -1,5 +1,19 @@
+# method 1 - uses selenium
+
 import os
 from pyhtml2pdf import converter
 
 path = os.path.abspath('templates/pathway_human.html')
-converter.convert(f'file:///{path}', 'py_converted.pdf')
+print_options = {'paperWidth': 11,
+                'paperHeight': 8.5,
+                'marginTop': 0,
+                'marginBottom': 0,
+                'marginLeft': 0,
+                'marginRight': 0,
+                 'scale': 0.75}
+converter.convert(f'file:///{path}', 'py_converted.pdf', print_options=print_options)
+
+
+
+# method 2 - xhtml2pdf - uses reportlab backend
+# in console xhtml2pdf templates/pathway_human.html
