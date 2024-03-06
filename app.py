fc7fa0a12ce8cbb88f195fbe6cac047bb5339a25
Alex Goodell <alexgoodell@gmail.com>
2024-03-06 15:29:04 -0800

    deploying

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from IPython.display import clear_output
 import os
 
 from blabel import LabelWriter
-from sh import lp
+# from sh import lp
 import pandoc
 import pypandoc
 import urllib
@@ -17,40 +17,18 @@ from flask import send_file
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        user_input = request.form['text_input']
-        # Do something with user_input here
-        label_writer = LabelWriter("templates/pathway.html",
-                                   default_stylesheets=("templates/pathway.css",))
-        records = [
-            dict(sample_id="s01", sample_name=user_input),
-        ]
-        label_writer.write_labels(records, target='static/label.pdf')
-
-        # To get better resolution
-        zoom_x = 0.75  # horizontal zoom
-        zoom_y = 0.75  # vertical zoom
-        mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
-
-        doc = fitz.open("static/label.pdf")  # open document
-        for page in doc:  # iterate through the pages
-            pix = page.get_pixmap(matrix=mat)  # render page to an image
-            pix.save("static/label.png")  # store image as a PNG
-
-        return render_template('result.html', result=user_input)
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/print-confirmed', methods=['GET', 'POST'])
 def print_label():
-    lp_cp = lp.bake('-d')
+    # lp_cp = lp.bake('-d')
     printername = 'UPS_Thermal_2844'
     filename = 'static/label.pdf'
     file2print = filename
-    lp_cp(printername, file2print)
+    # lp_cp(printername, file2print)
     return render_template('index.html')
 
 
